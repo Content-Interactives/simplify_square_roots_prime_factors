a8ee588c71fe88f27401e0c46438641d9e35999a
JoelCCK12 <joel.cruz.intern@ck12.org>
2025-06-25 10:35:37 -0700

    initialized react in repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SimplifySqRtPrime from './components/SimplifySqRtPrime'
+
+
+function App() {
+  return (
+      <div className="App">
+        <SimplifySqRtPrime />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/SimplifySqRtPrime.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const SimplifySqRtPrime = () => {
+
+
+	return (
+        <div></div>
+)
+};
+
+
+export default SimplifySqRtPrime;
