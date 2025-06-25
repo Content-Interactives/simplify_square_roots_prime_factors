999f493f675c791b4de18b59460e842ebd5a9159
JoelCCK12 <joel.cruz.intern@ck12.org>
2025-06-25 10:56:14 -0700

    added base and assets

--- src/components/SimplifySqRtPrime.jsx
+++ src/components/SimplifySqRtPrime.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
-
+import React from 'react';
+import './SimplifySqRtPrime.css';
 
 const SimplifySqRtPrime = () => {
-
-
 	return (
-        <div></div>
-)
+		<div className="prime-factorization-outer">
+			<div className="prime-factorization-title">Prime Factorization</div>
+			<div className="prime-factorization-inner"></div>
+		</div>
+	);
 };
-
 
 export default SimplifySqRtPrime;
